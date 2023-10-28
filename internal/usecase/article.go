4f2d5fd71a20927d222b1f2e36beb7d30b0102e3
tobg8 <theo.beloeil.pro@gmail.com>
2023-10-28 18:09:10 +0200

    feat: fetch news articles & push to kafka topic

---
+++ internal/usecase/article.go
@@ -0,0 +1,83 @@
+package usecase
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/tobg8/crypto-viz/common"
+	"github.com/tobg8/crypto-viz/internal/repository"
+)
+
+var lastURL string // Declare a variable to store the last slug
+
+// HandleNews will fetch news from API transform and send them through kafka
+func HandleNews(k *repository.KafkaClient) error {
+	articles := repository.FetchArticles()
+	if len(*articles) == 0 {
+		return fmt.Errorf("no news to send")
+	}
+
+	var events []common.ArticleEvent
+	for _, v := range *articles {
+		event := transformArticleToEvent(v)
+		events = append(events, event)
+	}
+
+	newEvents := filterSentEvents(events, &lastURL)
+	if len(newEvents) == 0 {
+		return nil
+	}
+	lastURL = newEvents[0].URL
+
+	// Push News
+	err := k.PushArticles(newEvents)
+	if err != nil {
+		return fmt.Errorf("could not send news")
+	}
+
+	return nil
+}
+
+// filterSentEvents returns the events not processed by the producer
+func filterSentEvents(ae []common.ArticleEvent, url *string) []common.ArticleEvent {
+	var newEvents []common.ArticleEvent
+	for _, event := range ae {
+		if event.URL != *url {
+			newEvents = append(newEvents, event)
+		} else {
+			log.Print("There is no articles to send yet")
+			break
+		}
+	}
+
+	return newEvents
+}
+
+// transformArticleToEvent transform article fields
+func transformArticleToEvent(a common.ArticleAPI) common.ArticleEvent {
+	currencies := TransformCurrency(a.Currencies)
+	return common.ArticleEvent{
+		ID:          a.ID,
+		Kind:        a.Kind,
+		Source:      a.SourceAPI.Domain,
+		Title:       a.Title,
+		URL:         a.SourceAPI.Domain + "/" + a.Slug,
+		Currencies:  currencies,
+		PublishedAt: a.PublishedAt,
+		CreatedAt:   a.CreatedAt,
+	}
+}
+
+// TransformCurrency transform currency fields
+func TransformCurrency(c []common.CurrencyAPI) []common.CurrencyEvent {
+	var currencies []common.CurrencyEvent
+	for _, v := range c {
+		currencies = append(currencies, common.CurrencyEvent{
+			Code:  strings.ToLower(v.Code),
+			Title: v.Slug,
+		})
+	}
+
+	return currencies
+}
--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"sync"
 	"time"
 
 	"github.com/go-co-op/gocron"
 	"github.com/joho/godotenv"
-	"github.com/tobg8/crypto-viz/common"
+	"github.com/tobg8/crypto-viz/internal/usecase"
 	"github.com/tobg8/crypto-viz/producer"
-	"github.com/tobg8/crypto-viz/scrapper"
 )
 
 func main() {
@@ -31,61 +28,8 @@ func Init() {
 
 	// init cron jobs
 	scheduler := gocron.NewScheduler(time.UTC)
-	scheduler.Every(10).Seconds().Do(func() {
-		Scrap(kafkaClient)
+	scheduler.Every(30).Seconds().Do(func() {
+		usecase.HandleNews(kafkaClient)
 	})
 	scheduler.StartBlocking()
-}
-
-// Scrap scraps currencies variations
-func Scrap(kc *producer.KafkaClient) error {
-	// Use a wait group to handle concurrent data fetching
-	var wg sync.WaitGroup
-
-	// Use channels to receive the results
-	currenciesCh := make(chan []common.CurrencyEvent)
-	newCurrenciesCh := make(chan []common.CurrencyEvent)
-
-	fetchCurrencies := func(url string, destCh chan<- []common.CurrencyEvent) {
-		defer wg.Done()
-		curr, err := scrapper.ScrapCurrencies(url)
-		if err != nil {
-			log.Printf("Error fetching currencies from %s: %v", url, err)
-			destCh <- nil
-			return
-		}
-		destCh <- curr
-	}
-
-	fetchNewCurrencies := func(url string, destCh chan<- []common.CurrencyEvent) {
-		defer wg.Done()
-		newCurr, err := scrapper.ScrapNewCurrencies(url)
-		if err != nil {
-			log.Printf("Error fetching new currencies from %s: %v", url, err)
-			destCh <- nil
-			return
-		}
-		destCh <- newCurr
-	}
-
-	// Fetch currencies and newCurrencies
-	wg.Add(2)
-	go fetchCurrencies("https://www.coingecko.com/fr", currenciesCh)
-	go fetchNewCurrencies("https://www.coingecko.com/fr/new-cryptocurrencies", newCurrenciesCh)
-
-	go func() {
-		wg.Wait()
-		close(currenciesCh)
-		close(newCurrenciesCh)
-	}()
-
-	currencies := <-currenciesCh
-	newCurrencies := <-newCurrenciesCh
-
-	// Create and send messages with the producer
-	if err := kc.PushCurrencyEvents(append(currencies, newCurrencies...)); err != nil {
-		return fmt.Errorf("failed to push currency events: %w", err)
-	}
-
-	return nil
 }
