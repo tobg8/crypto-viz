556fa58d25c0858ead2365ce19df67d875815a3e
tobg <71645695+tobg8@users.noreply.github.com>
2024-01-12 03:46:47 +0100

    fix(listing): fix listing missing event + modify cron jobs times (#9)

--- internal/usecase/listing.go
+++ internal/usecase/listing.go
@@ -2,13 +2,9 @@ package usecase
 
 import (
 	"fmt"
-	"log"
 
-	"github.com/tobg8/crypto-viz/common"
 	"github.com/tobg8/crypto-viz/internal/repository"
 )
-
-var lastBTCValue float64
 
 // HandleListing will currency listing from API and send them through kafka
 func HandleListing(k *repository.KafkaClient) error {
@@ -17,21 +13,10 @@ func HandleListing(k *repository.KafkaClient) error {
 	if len(*listing) == 0 {
 		return fmt.Errorf("no new listing")
 	}
-	newListing := isListingNew(*listing, &lastBTCValue)
-	if !newListing {
-		log.Print("no new listing to send")
-		return nil
-	}
 
-	lastBTCValue = (*listing)[0].CurrentPrice
 	err := k.PushListing(*listing)
 	if err != nil {
 		return fmt.Errorf("could not send listing: %w", err)
 	}
 	return nil
-}
-
-// isListingNew returns whether the listing has already been processed
-func isListingNew(l []common.ListingEvent, price *float64) bool {
-	return l[0].CurrentPrice != *price
 }
